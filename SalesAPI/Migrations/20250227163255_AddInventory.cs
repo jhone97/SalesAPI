71327aebef7f605287b7093d00228af136395870
Ali Sayed(ROG FLow) <sayed97.as@outlook.com>
2025-02-27 19:35:03 +0300

    final commit before testing api endpoints :)

---
+++ SalesAPI/Migrations/20250227163255_AddInventory.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stocks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InventoryId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "Items",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_InventoryId",
+                table: "Items",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_CompanyId",
+                table: "Inventories",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Inventories_InventoryId",
+                table: "Items",
+                column: "InventoryId",
+                principalTable: "Inventories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Inventories_InventoryId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_InventoryId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "InventoryId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Items");
+
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                });
+        }
+    }
+}
